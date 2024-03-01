1cf0692c8045cef7b6b17c8c03dc3ba2873400ad
Jan Behrens <jan.kristof.behrens@cvut.cz>
2024-03-01 11:55:48 +0100

    adjust tests to new signature

--- tests/test_error_case.py
+++ tests/test_error_case.py
@@ -1,20 +1,23 @@
 from control.control_logic import ControlLogic
+from methods.overlap_method import OverlapSchedule
 
 
 
 def test_model_invalid():
+    METHOD = OverlapSchedule
     case = '0'
-    execute_job = ControlLogic(case, distribution_seed=3, schedule_seed=0, sim_seed=0)
+    execute_job = ControlLogic(method=METHOD, case=case, distribution_seed=3, schedule_seed=0, sim_seed=0)
     execute_job.run(animation=True)
 
 
 
 def test_case():
+    METHOD = OverlapSchedule
     case = '5'
-    execute_job = ControlLogic(case, distribution_seed=3, schedule_seed=0, sim_seed=0)
+    execute_job = ControlLogic(method=METHOD, case=case, distribution_seed=3, schedule_seed=0, sim_seed=0)
     execute_job.run(animation=True)
 
-    execute_job = ControlLogic(case, distribution_seed=1, schedule_seed=0, sim_seed=0)
+    execute_job = ControlLogic(method=METHOD, case=case, distribution_seed=1, schedule_seed=0, sim_seed=0)
     execute_job.run(animation=True)
 
 
