e3b9ee6bbbda328cb584e3411eb7b856682a743b
Jan Behrens <jan.kristof.behrens@cvut.cz>
2024-03-05 22:49:39 +0100

    exp script with all params

--- exp_scripts/run_base_scheduling_exps.py
+++ exp_scripts/run_base_scheduling_exps.py
@@ -1,8 +1,11 @@
 from itertools import product
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterable
 from dask_jobqueue.slurm import SLURMCluster
 from distributed import Client, Future
+from dask.distributed import wait, progress
+import tqdm
+
 from methods import OverlapSchedule, RandomAllocation, MaxDuration
 
 from utils.file_handler import EnhancedJSONEncoder
@@ -22,9 +25,44 @@ class ExperimentSettings(BaseModel):
     run_on_cluster: bool = False
     num_workers: int = 2
 
-    exp_folder: Path = Path(__file__).parent.joinpath("experiments/")
+    exp_folder: Path = Path(__file__).parent.joinpath("experiments/base_sched")
     cases: list[int] = []
 
+    dist_seed: int = 10 
+    schedule_seed: int = 100
+    sim_seed: int = 1
+
+    det_job: list[bool] = [True, False]
+
+    variation_int_var_lst: list[str] = ["dist_seed", "schedule_seed", "sim_seed"]
+    variation_list_var_lst: list[str] = ["det_job"]
+    
+
+    def get_iter(self) -> Iterable:
+
+        attr_list = [getattr(self, attr) for attr in self.variation_int_var_lst]
+        range_lst = list(map(
+            range, attr_list
+        ))
+        lst_attr_list = [getattr(self, attr) for attr in self.variation_list_var_lst]
+        seed_iter = product(*range_lst, *lst_attr_list)
+
+        return seed_iter
+
+    # seed_iter = product(range(10), range(10), range(10))
+    #         for dist_seed, schedule_seed, sim_seed in seed_iter:
+
+
+class ExperimentRun(BaseModel):
+    case: int
+
+    dist_seed: int
+    schedule_seed: int
+    sim_seed: int
+    answer_seed: int
+    method_name: str
+
+    det_job: bool
 
 # settings = ExperimentSettings()
 # s = json.dumps(settings.dict(), cls=EnhancedJSONEncoder)
@@ -56,19 +94,30 @@ def start_cluster(exp_settings: ExperimentSettings) -> Client:
 
 def run_exp(
     method: Solver.__class__,
-    case: int,
-    dist_seed: int,
-    schedule_seed: int,
-    sim_seed: int,
-    answer_seed: int,
+    # case: int,
+    # dist_seed: int,
+    # schedule_seed: int,
+    # sim_seed: int,
+    # answer_seed: int,
+    # det_job: bool,
+    ** kwargs
 ) -> tuple[list[Any], dict[str, Any]]:
+    
+    case: int = kwargs["case"]
+    dist_seed: int = kwargs["dist_seed"]
+    schedule_seed: int = kwargs["schedule_seed"]
+    sim_seed: int = kwargs["sim_seed"]
+    answer_seed: int = kwargs["answer_seed"]
+    det_job: bool = kwargs["det_job"]
+
+
 
     method = method
-    case = case
-    dist_seed = dist_seed
-    sim_seed = sim_seed
-    schedule_seed = schedule_seed
-    answer_seed = answer_seed
+    # case = case
+    # dist_seed = dist_seed
+    # sim_seed = sim_seed
+    # schedule_seed = schedule_seed
+    # answer_seed = answer_seed
     job = Job(case, seed=dist_seed)
 
     agent_names = ["Human", "Robot"]
@@ -82,6 +131,8 @@ def run_exp(
                 answer_seed=answer_seed,
             )
         )
+    if det_job:
+        job = agents[0]._get_deterministic_job()
 
     solving_method = method(job=job, seed=schedule_seed)
     solving_method.prepare()
@@ -94,6 +145,17 @@ def run_exp(
         statistics["FAIL"] = True
 
     return schedule, statistics
+
+
+def save_data(exp_folder: Path, fname: str, schedule_stats: tuple[Any, Any], settings: dict[str, Any]):
+    schedule, stats = schedule_stats
+    with open(exp_folder.joinpath(fname), "w") as outfile:
+        # schedule, stats = res
+        # settings = run_settings[fname]
+        json.dump(out:={"schedule": schedule, "statistics": stats, "settings": settings}, outfile)
+        logging.info(f"Save data to {fname}")
+
+    return out
 
 
 def run_exps(client: Client, exp_settings: ExperimentSettings):
@@ -111,23 +173,71 @@ def run_exps(client: Client, exp_settings: ExperimentSettings):
         print(f"case: {case}")
         for method in methods:
             futures: dict[str, Future] = {}
-            seed_iter = product(range(10), range(10), range(10))
-            for dist_seed, schedule_seed, sim_seed in seed_iter:
-                answer_seed = sim_seed
+            run_settings: dict[str, ExperimentRun] = {}
+
+            # seed_iter = product(range(10), range(10), range(10))
+            seed_iter = exp_settings.get_iter()
+            for seeds_tpl in seed_iter:
+                settings = dict(zip(exp_settings.variation_int_var_lst + exp_settings.variation_list_var_lst, seeds_tpl))
+                settings["answer_seed"] = settings["sim_seed"]
                 # job = Job(case, seed=dist_seed)
-                fname = f"sched_case_{case}_method_{method.name()}_dist_{dist_seed}_sim_{sim_seed}_schedule_{schedule_seed}.json"
+                fname = f"sched_case_{case}_method_{method.name()}"
+                for name, seed in settings.items():
+                    fname += f"_{name}_{seed}"
+                fname += ".json"
+
+                settings["method_name"] = method.name()
+                settings["case"] = case
+
+                settings = ExperimentRun(**settings)
+                
+                # fname = f"sched_case_{case}_method_{method.name()}_dist_{dist_seed}_sim_{sim_seed}_schedule_{schedule_seed}.json"
                 if exp_folder.joinpath(fname).is_file():
                     continue
 
-                # res = client.submit(run_exp, job, 0, 0)
-                res: Future = client.submit(run_exp, method, case, dist_seed, schedule_seed, sim_seed, answer_seed)
-                futures[fname] = res
+                # res = run_exp(method=method, **settings)
+                res: Future = client.submit(run_exp, method, **settings.dict())
+                
+                future_saving = client.submit(save_data, exp_folder, fname, res, settings.dict())
 
-            for fname, res in futures.items():
-                with open(exp_folder.joinpath(fname), "w") as outfile:
-                    schedule, stats = res.result()
-                    json.dump({"schedule": schedule, "statistics": stats}, outfile)
-                    logging.info(f"Save data to {fname}")
+                # res: Future = client.submit(run_exp, method, case, dist_seed, schedule_seed, sim_seed, answer_seed)
+                futures[fname] = future_saving
+                run_settings[fname] = settings
+
+            print("progress")
+            # progress(futures.values())
+            batch_completion = 0
+            done, not_done = wait(list(futures.values()), return_when='FIRST_COMPLETED')
+            # for fn, future in futures.items():
+            batch_size = len(done) + len(not_done)
+            batch_completion += len(done)
+
+            del futures
+            del run_settings
+            with tqdm.tqdm(total=batch_size) as pbar:
+                while len(not_done) > 0:
+                    done, not_done = wait(not_done, return_when='FIRST_COMPLETED')
+                    batch_completion += len(done)
+                    # cur_perc = batch_completion / batch_size
+
+                    pbar.update(batch_completion)
+                    # print(f"completed work: {batch_completion} / {batch_size}")
+                
+            
+            print(f"batch: case {case}, method {method} completed.")
+
+           
+
+
+
+
+            # for fname, res in futures.items():
+            #     schedule, stats = res.result()
+            #     with open(exp_folder.joinpath(fname), "w") as outfile:
+            #         # schedule, stats = res
+            #         settings = run_settings[fname]
+            #         json.dump({"schedule": schedule, "statistics": stats, "settings": settings.dict()}, outfile)
+            #         logging.info(f"Save data to {fname}")
 
     # save original schedule, simulation with the same seed as in schedule, simulation with the another seed
     # statistics is saved for each simulation:
@@ -135,8 +245,6 @@ def run_exps(client: Client, exp_settings: ExperimentSettings):
     # - list of the rejection tasks with time, when it was rejected
     # - list with the rescheduling info: when, status of solver, makespan, calculation time
     # - list with the rescheduling evaluation info: when, makespan, calculation time
-
-    
 
     client.shutdown()
 
