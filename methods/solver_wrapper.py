6175afe63729432e0e143d77e88cf6e1990671e7
Jan Behrens <jan.kristof.behrens@cvut.cz>
2024-03-07 14:22:42 +0100

    adding solverwrapper to make searches reproducable

---
+++ methods/solver_wrapper.py
@@ -0,0 +1,144 @@
+import numpy as np
+from ortools.sat.python.cp_model import CpModel, CpSolver  # type: ignore
+from ortools.sat.python import cp_model  # type: ignore
+from pydantic import BaseModel, validator  # type: ignore
+
+from google.protobuf import text_format
+from ortools.sat import cp_model_pb2  # type: ignore
+from ortools.sat import sat_parameters_pb2  # type: ignore
+
+# from ortools.sat.python import swig_helper
+
+
+# class SolverConfig(BaseModel):
+#     # params: bytes
+#     max_time_in_seconds: float  # 10
+#     search_branching: str  # AUTOMATIC_SEARCH
+#     enumerate_all_solutions: bool  # true
+
+#     @classmethod
+#     def from_str(cls, message: str) -> "SolverConfig":
+#         lines = message.split("\n")
+#         key_val_arr = np.array(
+#             [
+#                 (line.split(":")[0], line.split(":")[1].replace(" ", ""))
+#                 for line in lines
+#                 if ":" in line and line.split(":")[1].__len__() > 0
+#             ]
+#         )
+#         parsed_dict = dict(zip(key_val_arr[:, 0], key_val_arr[:, 1]))
+#         return cls(**parsed_dict)
+
+
+# SolverConfig(enumerate_all_solutions=" true", max_time_in_seconds="10.4", search_branching="AUTOMATIC_SEARCH")
+
+
+class SolverResponseStats(BaseModel):
+    status: str  # OPTIMAL
+    objective: int  # 0
+    best_bound: int  # 0
+    integers: int  # 1
+    booleans: int  # 42
+    conflicts: int  # 0
+    branches: int  # 42
+    propagations: int  # 42
+    integer_propagations: int  # 84
+    restarts: int  # 42
+    lp_iterations: int  # 0
+    walltime: float  # 0.000574899
+    usertime: float  # 0.000574933
+    deterministic_time: float  #  6.64e-06
+    gap_integral: float  # 0
+    solution_fingerprint: int  # 0xf4751fa5d4643aaa
+
+    @validator("solution_fingerprint", pre=True, always=True)  # type: ignore
+    def set_solution_fingerprint(cls, v: int | str):
+        return int(v, 16) if isinstance(v, str) else int(v)
+
+    @classmethod
+    def from_str(cls, message: str) -> "SolverResponseStats":
+        lines = message.split("\n")
+        key_val_arr = np.array(
+            [
+                line.split(":")
+                for line in lines
+                if ":" in line and line.split(":")[1].__len__() > 0
+            ]
+        )
+        parsed_dict = dict(zip(key_val_arr[:, 0], key_val_arr[:, 1]))
+        return cls(**parsed_dict)
+
+
+class SolverRunLog(BaseModel):
+    model: str
+    solver_config: str
+    response: str
+    stats: SolverResponseStats
+
+    def get_model(self):
+        model = cp_model_pb2.CpModelProto()
+        assert text_format.Parse(self.model, model)
+
+        pmodel = CpModel()
+        pmodel.Proto().CopyFrom(model)
+
+        return pmodel
+
+    def get_response(self) -> cp_model_pb2.CpSolverResponse:
+        response_model = cp_model_pb2.CpSolverResponse()
+        assert text_format.Parse(self.response, response_model)
+        return response_model
+
+    def get_solver(self):
+        solver = SolverWrapper()
+        params = sat_parameters_pb2.SatParameters()
+        assert text_format.Parse(self.solver_config, params)
+        solver.parameters.CopyFrom(params)
+        return solver
+
+
+class SolverWrapper(CpSolver):
+    """A wrapper that logs the search process according to the settings."""
+
+    def Solve(
+        self,
+        model: CpModel,
+        solution_callback: cp_model.CpSolverSolutionCallback | None = None,
+    ) -> cp_model_pb2.CpSolverStatus:
+        solver_conf = text_format.MessageToString(self.parameters, as_utf8=True)
+        model_str = text_format.MessageToString(model.Proto(), as_utf8=True)
+
+        status = super().Solve(model=model, solution_callback=solution_callback)
+
+        self.solver_run_log = SolverRunLog(
+            model=model_str,
+            solver_config=solver_conf,
+            response=text_format.MessageToString(self.ResponseProto(), as_utf8=True),
+            stats=SolverResponseStats.from_str(self.ResponseStats()),
+        )
+
+        return status
+
+    def get_run_log(self) -> SolverRunLog:
+        """returns a configuration that contains the solver configuration and the model to reproduce the run
+
+        Returns:
+            SolverRunLog: the log that can be used to retrieve the model and/or rerun  the solve.
+        """
+        try:
+            return self.solver_run_log
+        except AttributeError as e:
+            raise RuntimeError(
+                "No Log available because the solver did not run. Call Solve first."
+            )
+
+    @staticmethod
+    def get_solver() -> "SolverWrapper":
+        solver = SolverWrapper()
+        # solver.parameters.num_search_workers = 1
+        # solver.parameters.random_seed = 42
+        solver.parameters.max_time_in_seconds = 10.0
+        solver.parameters.enumerate_all_solutions = True
+        # solver.parameters.log_search_progress = True if logging.getLogger().level == 10 else False
+        solver.parameters.search_branching = cp_model.AUTOMATIC_SEARCH
+        return solver
---
+++ tests/test_solver_logging.py
@@ -0,0 +1,136 @@
+
+import json
+from methods.solver_wrapper import SolverRunLog, SolverWrapper, CpModel, CpSolver
+from ortools.sat.python import cp_model, cp_model_helper
+
+from google.protobuf import text_format
+from ortools.sat import cp_model_pb2
+from ortools.sat import sat_parameters_pb2
+from ortools.sat.python import swig_helper
+
+
+def test_solve_save_reload():
+    solver = SolverWrapper.get_solver()
+    model = CpModel()
+
+    x: cp_model.IntVar = model.NewIntVar(0, 42, "x")
+
+    status = solver.Solve(model=model)
+    solver_conf = solver.get_run_log()
+    
+    assert status in [cp_model.OPTIMAL, cp_model.FEASIBLE]
+
+    x_val: int = solver.Value(x)
+
+    assert 0 <= x_val and x_val <= 42
+
+    runlog_json = solver.get_run_log().json()
+
+    run_log: SolverRunLog = SolverRunLog(**json.loads(runlog_json))
+
+    print(run_log)
+
+    # reproduce a search
+    logged_model = run_log.get_model()
+    assert logged_model.Proto() == model.Proto()
+
+    configured_solver = run_log.get_solver()
+    status = configured_solver.Solve(logged_model)
+
+    # cheack that the solution is same
+    assert solver_conf.stats.solution_fingerprint == configured_solver.get_run_log().stats.solution_fingerprint
+
+    # start again from that model
+    logged_model = run_log.get_model()
+    assert logged_model.Proto() == model.Proto()
+
+    # change the model by adding an objective
+    logged_model.Maximize(logged_model.GetIntVarFromProtoIndex(0))
+
+    configured_solver = run_log.get_solver()
+    status = configured_solver.Solve(logged_model)
+
+    # check that the solution is different
+    assert not solver_conf.stats.solution_fingerprint == configured_solver.get_run_log().stats.solution_fingerprint
+
+
+def test_make_model_from_string_and_back():
+    """ adapted from https://github.com/google/or-tools/blob/d37317b17ca16658451cafe05085fc22c39dd6c8/ortools/sat/python/swig_helper_test.py#L93
+    testing how to save and load models, configurations and responses to human readble strings.
+    """
+    model_string = """
+      variables { domain: -10 domain: 10 }
+      variables { domain: -10 domain: 10 }
+      variables { domain: -461168601842738790 domain: 461168601842738790 }
+      constraints {
+        linear {
+          vars: 0
+          vars: 1
+          coeffs: 1
+          coeffs: 1
+          domain: -9223372036854775808
+          domain: 9223372036854775807
+        }
+      }
+      constraints {
+        linear {
+          vars: 0
+          vars: 1
+          vars: 2
+          coeffs: 1
+          coeffs: 2
+          coeffs: -1
+          domain: 0
+          domain: 9223372036854775807
+        }
+      }
+      objective {
+        vars: -3
+        coeffs: 1
+        scaling_factor: -1
+      }"""
+    model = cp_model_pb2.CpModelProto()
+    assert text_format.Parse(model_string, model)
+
+    model_string2 = text_format.MessageToString(model, as_utf8=True)
+    model2 = cp_model_pb2.CpModelProto()
+    assert text_format.Parse(model_string2, model2)
+    
+    assert model == model2
+
+    parameters = sat_parameters_pb2.SatParameters()
+    parameters.optimize_with_core = True
+
+    param_str = text_format.MessageToString(parameters, as_utf8=True)
+    parameters2 = sat_parameters_pb2.SatParameters()
+    text_format.Parse(param_str, parameters2)
+    assert parameters2 == parameters
+
+    pmodel = CpModel()
+    pmodel.Proto().CopyFrom(model)
+
+    var0 = pmodel.GetIntVarFromProtoIndex(0)
+    var0.Proto()
+
+    solver = CpSolver()
+    solver.parameters = parameters
+
+    # solver.parameters.ParseFromString(parameters.SerializeToString())
+
+    # standard python solve
+    status = solver.Solve(pmodel)
+
+    # solve model via swig
+    solve_wrapper = swig_helper.SolveWrapper()
+    solve_wrapper.SetParameters(parameters)
+    solution = solve_wrapper.Solve(model)
+
+    assert cp_model_pb2.OPTIMAL == solution.status
+    assert 30.0 == solution.objective_value
+
+    assert cp_model.OPTIMAL == status
+    assert 30.0 == solver.ObjectiveValue()
+
+
+
+
